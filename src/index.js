1ccc82dfa38cc045c4dcdcf8fe87e8be4de87f97
Jason Yu <me@ycmjason.com>
2018-01-15 20:39:27 +0000

    removed requireAll to allow deployment

--- src/index.js
+++ src/index.js
@@ -11,8 +11,8 @@ const registerAll = (context, prefix) => context.keys().forEach(p => {
   Vue.component(prefix + name, context(p).default);
 });
 
-registerAll(require.context('@/components', false, /.*\.vue/), 'sp');
-registerAll(require.context('@/components_presentational', false, /.*\.vue/), 'spP');
+// registerAll(require.context('@/components', false, /.*\.vue/), 'sp');
+// registerAll(require.context('@/components_presentational', false, /.*\.vue/), 'spP');
 
 new Vue({
   router,
