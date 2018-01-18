bc570b5da5c7989e68a16d83642b2d20559167a1
Jason Yu <me@ycmjason.com>
2018-01-18 21:36:40 +0000

    added header and footer components

---
+++ src/components/Footer.vue
@@ -0,0 +1,13 @@
+<template>
+  <footer>
+    <main class="container">
+      &copy; {{Date.now()}}
+    </main>
+  </footer>
+</template>
+
+<script>
+export default { }
+</script>
+
+<style scoped></style>
---
+++ src/components/Header.vue
@@ -0,0 +1,24 @@
+<template>
+  <header>
+    <main class="container">
+      <h1>snappen.io</h1>
+    </main>
+  </header>
+</template>
+
+<script>
+export default { }
+</script>
+
+<style lang="scss" scoped>
+@import '../scss/config';
+
+h1 {
+  margin: 0;
+}
+
+header {
+  padding-top: $core-length;
+  padding-bottom: $core-length;
+}
+</style>
